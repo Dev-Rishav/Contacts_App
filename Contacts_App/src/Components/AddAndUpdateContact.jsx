43593818c197eca409bb05fc59f8f63743a7cd50
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-18 01:52:46 +0530

    Inject form into AddAndUpdateContact components using formik

--- Contacts_App/src/Components/AddAndUpdateContact.jsx
+++ Contacts_App/src/Components/AddAndUpdateContact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import {Field, Formik,Form} from "formik";
 import Modal from './Modal'
 
 const AddAndUpdateContact = ({isOpen,onClose}) => {
@@ -8,7 +8,34 @@ const AddAndUpdateContact = ({isOpen,onClose}) => {
         isOpen={isOpen} 
         onClose={onClose}
       >
-        yelo
+       
+        <Formik>
+            <Form className="flex flex-col gap-4">
+                <div className="flex flex-col gap-1">
+                <lable htmlFor="name">
+                    Name
+                </lable>
+                <Field name="name" className="h-10 border px rounded-lg "/>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                <lable htmlFor="email">
+                    Email
+                </lable>
+                <Field type="email" name="name" className="h-10 border rounded-lg"/>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                <lable htmlFor="Phone_No">
+                    Phone Number
+                </lable>
+                <Field name="phone" className="h-10 border rounded-lg"/>
+                </div>
+
+                <button className="bg-orange px-3 py-1.5 border self-end rounded-lg ">Add Contact</button>
+            </Form>
+        </Formik>
+
       </Modal>
     </div>
   )
