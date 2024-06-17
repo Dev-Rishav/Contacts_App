34cad2322b7fb1f3340d47667b0ecca66701370f
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-18 02:06:32 +0530

    configure create functionality with DB

--- Contacts_App/src/Components/AddAndUpdateContact.jsx
+++ Contacts_App/src/Components/AddAndUpdateContact.jsx
@@ -1,44 +1,70 @@
-import {Field, Formik,Form} from "formik";
+import { Field, Formik, Form } from "formik";
 import Modal from './Modal'
+import { addDoc, collection } from "firebase/firestore";
+import {db} from "../config/firebase"
 
-const AddAndUpdateContact = ({isOpen,onClose}) => {
-  return (
-    <div>
-        <Modal
-        isOpen={isOpen} 
-        onClose={onClose}
-      >
-       
-        <Formik>
-            <Form className="flex flex-col gap-4">
-                <div className="flex flex-col gap-1">
-                <lable htmlFor="name">
-                    Name
-                </lable>
-                <Field name="name" className="h-10 border px rounded-lg "/>
-                </div>
+const AddAndUpdateContact = ({ isOpen, onClose }) => {
 
-                <div className="flex flex-col gap-1">
-                <lable htmlFor="email">
-                    Email
-                </lable>
-                <Field type="email" name="name" className="h-10 border rounded-lg"/>
-                </div>
+    const addContact= async (contact) =>{
+        try {
+            
+            const contactRef = collection(db,"contacts");
+            await addDoc(contactRef,contact);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-                <div className="flex flex-col gap-1">
-                <lable htmlFor="Phone_No">
-                    Phone Number
-                </lable>
-                <Field name="phone" className="h-10 border rounded-lg"/>
-                </div>
 
-                <button className="bg-orange px-3 py-1.5 border self-end rounded-lg ">Add Contact</button>
-            </Form>
-        </Formik>
 
-      </Modal>
-    </div>
-  )
+    return (
+        <div>
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+
+                <Formik
+                    initialValues={{
+                        name:"",
+                        Email: "",
+                        Phone_No:"",
+                    }}
+
+                    onSubmit={values => {       //this function triggers when the form is submitted
+                        console.log(values);
+                        addContact(values);
+                    }}
+                >
+                    <Form className="flex flex-col gap-4">
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="name">
+                                Name
+                            </label>
+                            <Field name="name" className="h-10 border px rounded-lg " />
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="email">
+                                Email
+                            </label>
+                            <Field type="email" name="Email" className="h-10 border rounded-lg" />
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="Phone_No">
+                                Phone Number
+                            </label>
+                            <Field name="Phone_No" className="h-10 border rounded-lg" />
+                        </div>
+
+                        <button className="bg-orange px-3 py-1.5 border self-end rounded-lg ">Add Contact</button>
+                    </Form>
+                </Formik>
+
+            </Modal>
+        </div>
+    )
 }
 
 export default AddAndUpdateContact
