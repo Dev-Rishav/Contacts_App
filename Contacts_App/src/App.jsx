0359be94b09dbdaf5e793b2e6b5cac40743b2ff8
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-17 14:09:29 +0530

    Beautify Navbar and introduce search box

--- Contacts_App/src/App.jsx
+++ Contacts_App/src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css';
-import Navbar from './Components/Navbar'
+import Navbar from './Components/Navbar';
+import { MdPersonSearch } from "react-icons/md";
+import { FaPlusCircle } from "react-icons/fa";
 
 function App() {
 
   return (
     <>
-    <div>
-      <Navbar/>
-    </div>
+      <div className='max-w-[370px] mx-auto px-4'>
+        <Navbar />
+        <div className='flex'>
+        <div className='relative flex items-center flex-grow'>
+          <MdPersonSearch className='text-3xl text-white absolute ml-1'></MdPersonSearch>
+          <input type="text" className='border text-white pl-9 bg-transparent border-white h-10 flex-grow rounded-md '></input>
+        </div>
+        <FaPlusCircle className='text-4xl ml-2 cursor-pointer text-white gap-2'></FaPlusCircle>
+      </div>
+    </div >
     </>
   )
 }
--- Contacts_App/src/Components/Navbar.jsx
+++ Contacts_App/src/Components/Navbar.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='my-4 flex items-center gap-4 text-xl font-medium justify-center h-[60px] bg-white m-4 rounded-lg'>
+            <h1>Contacts App</h1>
+
+    </div>
   )
 }
 
