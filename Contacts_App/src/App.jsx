610a3bf4bbba23c1ebd1349a8167e5766893c0ef
Dev-Rishav <rishavchakraborty94@gmail.com>
2024-06-17 15:31:11 +0530

    Cultivate db data to the frontend

--- Contacts_App/src/App.jsx
+++ Contacts_App/src/App.jsx
@@ -1,22 +1,29 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import { MdPersonSearch } from "react-icons/md";
-import { FaPlusCircle } from "react-icons/fa";
+import { FaPlusCircle, FaUser } from "react-icons/fa";
+import { IoMdTrash } from "react-icons/io";
+import { RiEditCircleLine } from "react-icons/ri";
 import { useEffect, useState } from 'react';
-import {collection, getDocs} from  "firebase/firestore";
-import {db} from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/firebase";
 
 function App() {
 
-  const [contact,setContact]= useState([]); //Initialize an empty array
+  const [contact, setContact] = useState([]); //Initialize an empty array
 
-  useEffect( () => {
+  useEffect(() => {
     const getContacts = async () => {
       try {
-        const contactRef = collection(db,"contacts");
+        const contactRef = collection(db, "contacts");
         const contactSnapshot = await getDocs(contactRef);
-        const contactList = contactSnapshot.docs.map((doc)=> doc.data());
-        console.log(contactList);
+        const contactList = contactSnapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        setContact(contactList); //update default list
       } catch (error) {
         console.log(error);
       }
@@ -24,23 +31,42 @@ function App() {
 
     getContacts();
 
-  },[]);
+  }, []);
 
 
   return (
     <>
       <div className='max-w-[370px] mx-auto px-4'>
         <Navbar />
-        <div className='flex'>
-        <div className='relative flex items-center flex-grow'>
-          <MdPersonSearch className='text-3xl text-white absolute ml-1'></MdPersonSearch>
-          <input type="text" className='border text-white pl-9 bg-transparent border-white h-10 flex-grow rounded-md '></input>
+        <div className='flex gap-2'>
+          <div className='relative flex items-center flex-grow'>
+            <MdPersonSearch className='text-3xl text-white absolute ml-1'></MdPersonSearch>
+            <input type="text" className='border text-white pl-9 bg-transparent border-white h-10 flex-grow rounded-md '></input>
+          </div>
+          <FaPlusCircle className='text-4xl ml-2 cursor-pointer text-white gap-2'></FaPlusCircle>
         </div>
-        <FaPlusCircle className='text-4xl ml-2 cursor-pointer text-white gap-2'></FaPlusCircle>
-      </div>
-    </div >
-    </>
-  )
+        <div className='mt-5' >
+          {
+            contact.map(contact => (
+              <div key={contact.id} className='p-2 rounded-lg  bg-yellow flex justify-around items-center' >
+                <div className='flex gap-2'>
+                  <FaUser className='text-orange text-3xl' />
+                  <div className=''>
+                    <h2 className='font-medium'>{contact.name}</h2>
+                    <h2 className='text-sm font-medium'>{contact.Email}</h2>
+                    <h2 className='text-sm font-medium'>{contact.Phone_No}</h2>
+                  </div>
+                  <div className='flex gap-1 text-xl'>
+                    <RiEditCircleLine></RiEditCircleLine>
+                    <IoMdTrash></IoMdTrash>
+                  </div>
+                </div>
+              </div>
+        ))}
+              </div>
+      </div >
+      </>
+      )
 }
 
-export default App
+      export default App
